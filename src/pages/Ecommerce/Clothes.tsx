0d17ff86bc5b1b3495ae5d4970eda5ca449be3a8
chilemati <amadichile007@gmail.com>
2025-07-31 00:37:23 +0100

    ecommerce v1.11

--- src/pages/Ecommerce/Clothes.tsx
+++ src/pages/Ecommerce/Clothes.tsx
@@ -12,7 +12,7 @@ const Clothes = () => {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
         {products.map(product => (
         <div className='shadow-lg rounded-md flexCenter flex-col gap-1 py-2 ' key={product?.id}>
-          <Link className='w-full' to={`/e-commerce/clothe/${`details-of-user-${product?.name}_1`}`}>
+          <Link className='w-full' to={`/e-commerce/clothe/${`details-of-user-${product?.name}_${product?.id}`}`}>
            <img loading='lazy'src={product?.avatar} alt={product.name}  className='w-full h-[200px] rounded-md object-cover  ' /> 
             </Link>
           <h3 className='text-center' >{product?.name}</h3>
